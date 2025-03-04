d6086883c5472eab9d70b8dbd2bac1f054966ec1
Hiroki Kato <hirokikato1@gmail.com>
2025-03-04 15:23:23 -0500

    creates oscillating circle and updates notes.MD for explanation

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const myCanvas= document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
-const {width, height} = myCanvas.myCanvas;
+const {width, height} = myCanvas;
 
 
 ctx.translate(width/2, height/2);
 
-drawCircle(0,0);
+animate();
+
 
 
 function drawCircle(x,y,radius=20){
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI*2);
     ctx.fill();
+}
+
+function animate(){
+    ctx.clearRect(-width/2, -height/2, width, height)
+    const offset = Date.now()/1000;
+    
+    const x = Math.sin(offset) * width /2;
+    drawCircle(x,0)
+
+    requestAnimationFrame(animate)
+
 }
