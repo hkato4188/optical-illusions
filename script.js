5a528b79c12d69c5509f8269715e02dc3ee3b103
Hiroki Kato <hirokikato1@gmail.com>
2025-03-04 17:11:57 -0500

    creates snippets and comments for debugging and demonstration

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const myCanvas= document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 const {width, height} = myCanvas;
-const circleCount = 10;
+
+// increase and decrease for visual aid
+// const circleCount = 200;
+const circleCount = 20;
 
 ctx.translate(width/2, height/2);
 
@@ -11,22 +14,50 @@ function animate(){
     ctx.clearRect(-width/2, -height/2, width, height)
     
 
-    ctx.save();
+    
     for(let i=0; i<circleCount; i++){
         // spread circles by adding to offset based on PI
-        const offset = Date.now()/1000 + i * Math.PI / circleCount;
-        const x = Math.sin(offset) * height /2;
+        
+        // what if you do not add to the offset (fan out effect)? 
+        // const offset = Date.now()/1000;
+        
+        // what if you do not add a small number to the offset and multiply by i (curling effect)?
+        // const offset = Date.now()/1000 + 0.5 * i * Math.PI / circleCount;
 
-        ctx.rotate(Math.PI/circleCount)
+        // what if you do not add a large number to the offset and multiply by i (curling effect)?
+        // Odd numbers close the loop eliminates teh curcling effect
+        // const offset = Date.now()/1000 + 3 * i * Math.PI / circleCount;
+        
+        // Craziest
+        // const s = Math.sin(Date.now()/1000);
+        // const s = 1 +Math.sin(Date.now()/1000);
+        // const offset = Date.now()/1000 + s * i * Math.PI / circleCount;
+
+
+        // Code for circle
+        const offset = Date.now()/1000 + i * Math.PI / circleCount;
+        
+        
+        const x = Math.sin(offset) * height /2;
+        ctx.save();
+        ctx.rotate(i*Math.PI/circleCount)
 
         drawCircle(x,0);
+        // fill the middle circle for debugging
+        // if(i===circleCount/2) ctx.fill();
+        // fill the first circle
+        if(i===circleCount/2) ctx.fill();
+        ctx.restore();
     }    
-    ctx.restore();
+    
     requestAnimationFrame(animate);
 }
 
 function drawCircle(x,y,radius=20){
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI*2);
-    ctx.fill();
+    
+    // change ctx draw method to stroke and only fill one circle
+    ctx.stroke()
+    // ctx.fill();
 }
